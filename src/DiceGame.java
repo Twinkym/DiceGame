253b6d809704eda8e6fe695af2d2577e3eb84cd7
david de la puente enriquez <Twinkymen@hotmail.com>
2022-01-31 10:07:37 +0100

    Initial commit

---
+++ src/DiceGame.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class DiceGame {
+
+    public static void main (String[] args) {
+        int diceValue1=new Random().nextInt(1,7);
+//        int diceValue1=new Random().nextInt(6)+1;
+    }
+}
